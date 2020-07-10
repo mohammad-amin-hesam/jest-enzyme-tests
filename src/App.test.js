44fdc28c4e5f867a01da9b88d4f0f60f400e5a8a
amin <singlepageprogrammer@gmail.com>
2020-07-11 00:40:43 +0430

    part1

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,18 @@
 import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
-test("renders learn react link", () => {});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  //مشاهده کد به صورت رشته در کانسول
+  // console.log(wrapper.debug());
+  // باید درست باشه
+  // expect(wrapper).toBeTruthy();
+  // باید اشتباه باشه
+  // expect(wrapper).toBeFalsy();
+});
+
+// Part of 1 finished :) Ready for part2
