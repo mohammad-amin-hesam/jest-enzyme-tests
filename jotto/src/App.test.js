69b63295b78a0d179b1ed28978894dd850db52e0
amin <singlepageprogrammer@gmail.com>
2020-07-13 16:48:09 +0430

    finish part 3

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/*
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @pram {any} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+/*
+ *Return ShallowWrapper containing node(s) with the given data-test value
+ *@param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ *@param {string} val - Value of data-test attribute for search
+ *@returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
+test("renders app without errors", () => {});
---
+++ jotto/src/Congrats.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed message!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats"></div>;
+  }
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
+};
+
+export default Congrats;
+
+// part 3 finished start part 4
