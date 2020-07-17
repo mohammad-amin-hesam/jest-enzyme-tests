0b7c7cacb00b47d72d4ca4dc816c44822daab1a5
amin <singlepageprogrammer@gmail.com>
2020-07-17 14:25:25 +0430

    guessed words rows created

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -7,6 +7,30 @@ const GuessedWords = (props) => {
     contents = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => {
+      const key = `keywordItemRow${index}`;
+      return (
+        <tr key={key} data-test="guessed-word">
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>GuessedWords</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <ht>Matching Letters</ht>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{contents}</div>;
 };
