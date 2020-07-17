111d59c41aef6204b12d3e53a63098870cfdb0eb
amin <singlepageprogrammer@gmail.com>
2020-07-17 15:04:42 +0430

    passed test

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Jotto App</h1>
+      <Congrats success={false} />
+      <GuessedWords
+        guessedWords={[
+          {
+            guessedWord: "train",
+            letterMatchCount: 3,
+          },
+        ]}
+      />
     </div>
   );
 }
