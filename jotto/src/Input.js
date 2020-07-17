f991a2d8984fddc54726319db96c6c1f170e1704
amin <singlepageprogrammer@gmail.com>
2020-07-17 22:42:49 +0430

    part 9/5

---
+++ jotto/src/Input.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Input from "./Input";
+
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    it("renders component without error", () => {});
+    it("renders input box", () => {});
+    it("renders submit button", () => {});
+  });
+  describe("word has been guessed", () => {
+    it("renders component without error", () => {});
+    it("does not renders input box", () => {});
+    it("does not renders submit button", () => {});
+  });
+});
+
+describe("update state", () => {});
+
+// 9 from part 5 finished
---
+++ jotto/src/Input.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { connect } from "redux";
+
+const Input = () => {
+  return <div></div>;
+};
+
+export default connect((state) => state, {})(Input);
