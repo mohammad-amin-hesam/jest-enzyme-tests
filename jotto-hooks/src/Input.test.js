2097fe88ef8515bf256dcd55808037388b31443f
amin <singlepageprogrammer@gmail.com>
2020-10-13 21:09:26 +0330

    amin: part 9 finished

--- jotto-hooks/src/Input.test.js
+++ jotto-hooks/src/Input.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Input from "./Input";
-import { findByTestAttr } from "./test/testUtils";
+import { findByTestAttr, checkProps } from "./test/testUtils";
 
-const setup = () => {
-  return shallow(<Input />);
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
 };
 
 test("Input renders without error", () => {
@@ -12,4 +12,8 @@ test("Input renders without error", () => {
   const component = findByTestAttr(wrapper, "component-input");
 
   expect(component.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
 });
