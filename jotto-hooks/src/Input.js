08a42d83e5b37c90bf793c119f9e3d054af1211d
amin <singlepageprogrammer@gmail.com>
2020-10-13 22:15:56 +0330

    amin: part 3 from 10 finished

--- jotto-hooks/src/Input.js
+++ jotto-hooks/src/Input.js
@@ -1,8 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Input = () => {
-  return <div data-test="component-input"></div>;
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState("");
+
+  return (
+    <div data-test="component-input">
+      <form>
+        <input
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+          data-test="input-box"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurrentGuess("");
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
--- jotto-hooks/src/Input.test.js
+++ jotto-hooks/src/Input.test.js
@@ -17,3 +17,27 @@ test("Input renders without error", () => {
 test("does not throw warning with expected props", () => {
   checkProps(Input, { secretWord: "party" });
 });
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  beforeEach(() => {
+    // mockClear clears mock for every test. if we do not this it will be create in every test
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test("state updates with value of input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("field is cleared upon submit button click", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
+});
